84d89e3dde6a2d68927197b94405a9232df0a386
Utsav <utsavj502@gmail.com>
2022-01-22 20:41:21 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import About from './components/About';
 import Navbar from './components/Navbar';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 export default function App() {
@@ -11,9 +9,6 @@ export default function App() {
       <Navbar />
       <About />
       <Skills />
-{/* 
-      <Projects />
-      <Contact />*/}
     </main>  
 
   );
