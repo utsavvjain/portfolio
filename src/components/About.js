74164c27fb204eae3c1a01cd6f2dcf414cd8e196
Utsav <utsavj502@gmail.com>
2022-01-29 15:40:24 +0530

    Updated about section message

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
               Hi, I'm Utsav.
-              <br className="hidden lg:inline-block" />I am a software engineer
+              <br className="inline-block" />Full stack developer
             </h1>
             <p className="mb-8 leading-relaxed">               
             Having knowledge of Software Engineering which implies best practices and strong focus on clean code. Emphasis on practical opportunities to work with an organisation to enhance my knowledge, skills and to understand the technicalities of real-world environment.            </p>
