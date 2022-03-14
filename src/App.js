e6faf560675a9476906e6ca6ebde3837d2a0ff8f
Utsav <utsavj502@gmail.com>
2022-03-14 11:51:56 +0530

    Added support for google analytics

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import Footer from "./components/Footer";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 
+import ReactGA from 'react-ga'
+
 export default function App() {
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID)  
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
-   <main className="text-gray-400 bg-gray-900 body-font scroll-auto"> 
+    <main className="text-gray-400 bg-gray-900 body-font scroll-auto">
       <Navbar />
       <About />
-      <Experience/>
-      <Projects/>
+      <Experience />
+      <Projects />
       <Skills />
-      <Contact/>
-      <Footer/>
-    </main>  
+      <Contact />
+      <Footer />
+    </main>
 
   );
 }
